91dbf03a876f236f2bd740286ce14f8c2dbd4326
Josh Barr <josh@springload.co.nz>
2014-01-24 13:08:44 +1300

    removing unused files, updated checking for hide_in_nav

--- wrangler/Core.py
+++ wrangler/Core.py
@@ -307,7 +307,7 @@ class Page(object):
         return False;
 
     def show_in_navigation(self):
-        return False if "hide_from_nav" in self.data["meta"] else True
+        return False if "hide_from_nav" in self.data["meta"] and self.data["meta"]["hide_from_nav"] == "True" else True
 
     def get_weight(self):
         return self.data["meta"]["weight"] if "weight" in self.data["meta"] else 0
